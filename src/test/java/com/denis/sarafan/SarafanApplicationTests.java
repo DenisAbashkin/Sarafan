4918ee2ff44d64e914cba379c04f0a15b67f9597
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2022-03-02 17:48:10 +0300

    Initial commit

---
+++ src/test/java/com/denis/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denis.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
