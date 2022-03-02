9fdca97be8ee9114c18de792884c0b5c8dddcafb
DenisAbashkin <35079479+DenisAbashkin@users.noreply.github.com>
2022-03-02 22:34:49 +0300

    add exception NotFoundException

--- src/main/java/com/denis/sarafan/controller/MessageController.java
+++ src/main/java/com/denis/sarafan/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.denis.sarafan.controller;
 
+import com.denis.sarafan.exceptions.NotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,7 @@ public class MessageController {
         return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
-                .orElseThrow();
+                .orElseThrow(NotFoundException::new);
     }
 
 }
